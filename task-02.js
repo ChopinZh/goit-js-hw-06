f4e917c3737d18289214fb68fee2d99efcf38507
ChopinZh <73756802+ChopinZh@users.noreply.github.com>
2021-05-23 22:50:21 +0200

    Add files via upload

---
+++ task-02.js
@@ -0,0 +1,29 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+  
+  
+  const ulEl = document.querySelector('ul#ingredients');
+  const createListItem = ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+    liEl.style.listStyle = 'none';
+    liEl.insertAdjacentHTML(
+      'afterbegin', `<li>${ingredient}</li>`,
+    );
+    return liEl;
+  });
+  
+  ulEl.append(...createListItem);
+  
+  //const ulEl = document.querySelector('ul#ingredients');
+  //ingredients.forEach(ingredient => {
+  //    console.log(ingredient);
+  //    const liEl = document.createElement('li');
+  //    liEl.textContent = ingredient;
+  //ulEl.append(liEl);
+  //});
