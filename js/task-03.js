d3dd76c529e54eb600aab88477374034bc0a0bf8
ChopinZh <73756802+ChopinZh@users.noreply.github.com>
2022-05-01 23:15:19 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,26 +1,20 @@
 const images = [
-    {
-      url:
-        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
-  ];
-  
-  const ulRef = document.querySelector('#gallery');
-  ulRef.classList.add('gallery-list');
-  const makeGalleryImage = ({ url, alt }) => {
-      return `<li class="gallery-list-item" ><img class="gallery-list-img" src="${url}" alt="${alt}"></li>`;
-  }
-  
-  const makeGalleryImageList = images.map(makeGalleryImage).join('');
-  ulRef.insertAdjacentHTML('beforeend', makeGalleryImageList);
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const listRef = document.querySelector(".gallery")
+const itemMarkup = images.map(image => `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+listRef.insertAdjacentHTML('beforeEnd', itemMarkup)
+console.log(listRef)
