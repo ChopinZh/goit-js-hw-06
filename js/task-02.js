fb07a8c9f8b282ea82c6aec8946f0333bfb4c09b
ChopinZh <73756802+ChopinZh@users.noreply.github.com>
2022-05-01 23:12:53 +0200

    corrected in css file

--- js/task-02.js
+++ js/task-02.js
@@ -6,16 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
   ];
-  const ulRef = document.querySelector('#ingredients');
-  
-    const makeLiRef = array => {
-      return array.map(element => {
-  
-          const liRef = document.createElement('li');
-          liRef.textContent = element;
-          return liRef;
-      });
-  }
-  
-  const liList = makeLiRef(ingredients);
-  ulRef.append(...liList);
+  const listRef = document.querySelector('#ingredients')
+
+  const elements = ingredients.map(ingredient => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = ingredient;
+    itemEl.classList.add("item")
+  console.log(itemEl)
+    return itemEl;
+  })
+  listRef.append(...elements)
